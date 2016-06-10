eee3773f74e92ea3e812a67bb82886fb6889ea51
zaiddawoodissa@gmail.com <zaiddawoodissa@gmail.com>
2016-06-10 17:23:14 +0200

    first commit in AngularJSApps

---
+++ script.sh
@@ -0,0 +1,6 @@
+echo "# AngularJSApps" >> README.md
+git init
+git add *
+git commit -m "first commit in AngularJSApps"
+git remote add origin https://github.com/Nawter/AngularJSApps.git
+git push -u origin master
